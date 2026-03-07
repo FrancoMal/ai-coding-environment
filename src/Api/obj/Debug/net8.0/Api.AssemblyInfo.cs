34c98677c693fc74721948becf7e28fda241bf4f
Alejandro Malfetano <ajmalfe@users.noreply.github.com>
2026-03-07 00:31:21 -0300

    extra

--- src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ef68fd614595001b3e79040aa39ab60c4886f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f8e3e70d65dc2843155dd0872289889e38757d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
